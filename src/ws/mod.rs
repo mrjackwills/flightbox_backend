4a03aa1a80f7d8a41acc455622e58724bbbd2329
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2025-01-12 22:52:56 +0000

    chore: dependencies updated

--- src/ws/mod.rs
+++ src/ws/mod.rs
@@ -47,7 +47,7 @@ async fn incoming_ws_message(mut reader: WSReader, ws_sender: WSSender, mut auto
             Message::Text(message) => {
                 let ws_sender = C!(ws_sender);
                 tokio::spawn(async move {
-                    ws_sender.on_text(message).await;
+                    ws_sender.on_text(message.to_string()).await;
                 });
             }
             Message::Ping(_) => auto_close.on_ping(&ws_sender),
--- src/ws_messages/outgoing.rs
+++ src/ws_messages/outgoing.rs
@@ -31,7 +31,7 @@ impl StructuredResponse {
             unique,
             cache,
         };
-        Message::Text(serde_json::to_string(&x).unwrap_or_default())
+        Message::Text(serde_json::to_string(&x).unwrap_or_default().into())
     }
 
     /// Convert a ErrorResponse into a Tokio message of StructureResponse
@@ -42,6 +42,6 @@ impl StructuredResponse {
             unique,
             cache: None,
         };
-        Message::Text(serde_json::to_string(&x).unwrap_or_default())
+        Message::Text(serde_json::to_string(&x).unwrap_or_default().into())
     }
 }
