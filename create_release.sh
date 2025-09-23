bef1d7826fd0acf61b436f13b76853a218c35cfd
Jack Wills <32690432+mrjackwills@users.noreply.github.com>
2025-09-23 16:15:33 +0000

    chore: create_release.sh updated

--- create_release.sh
+++ create_release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# rust create_release v0.6.0
-# 2024-10-19
+# rust create_release v0.6.3
+# 2025-09-20
 
 STAR_LINE='****************************************'
 CWD=$(pwd)
@@ -214,13 +214,24 @@ cargo_build_aarch64_linux() {
 	cross build --target aarch64-unknown-linux-musl --release
 }
 
+cargo_clean() {
+	echo -e "${YELLOW}cargo clean${RESET}"
+	cargo clean
+}
+
 # Build all releases that GitHub workflow would
 # This will download GB's of docker images
-cargo_build_all() {
+# $1 is 0 or 1, if 1 won't run ask_continue
+cross_build_all() {
+	if ask_yn "cargo clean"; then
+		cargo_clean
+	fi
+	skip_confirm=$1
 	cargo_build_aarch64_linux
-	ask_continue
+	["$skip_confirm" -ne 1 ] && ask_continue
 	cross_build_x86_linux
-	ask_continue
+	[ "$skip_confirm" -ne 1 ] && ask_continue
+
 }
 
 # $1 text to colourise
@@ -258,7 +269,7 @@ release_flow() {
 	get_git_remote_url
 
 	cargo_test
-	cargo_build_all
+	cargo_build_all 0
 
 	cd "${CWD}" || error_close "Can't find ${CWD}"
 	check_tag
@@ -326,6 +337,7 @@ build_choice() {
 		1 "x86 musl linux" off
 		2 "aarch64 musl linux" off
 		3 "all" off
+		4 "all automatic" off
 	)
 	choices=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
 	exitStatus=$?
@@ -348,7 +360,11 @@ build_choice() {
 			;;
 	
 		3)
-			cargo_build_all
+			cargo_build_all 0
+			exit
+			;;
+		4)
+			cargo_build_all 1
 			exit
 			;;
 		esac
